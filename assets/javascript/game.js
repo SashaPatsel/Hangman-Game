3b3bb817aa35e35d86912f3d2d1a900c93e1b59f
Sasha Patsel <sashapatsel@sashas-mbp.attlocal.net>
2018-01-21 11:18:18 -0800

    CSS (mostly) done

---
+++ assets/javascript/game.js
@@ -0,0 +1,8 @@
+var hangman = ["Michael Jordan", "Kobe Bryant", "Stephen Curry", "Hakeem Olajuwon", "Shaquille O'Neal", "LeBron James", "Tim Duncan", "Russel Westbrook", "Greg Poppovich", "Charles Barkley"]
+
+
+// if (userInput === "k" || userInput === "o" || userInput === "b" || userInput === "e") {
+
+// }
+
+//Will need to use toLowerCase()
