4f1afd355caf579bf10ee512ec7a849486699f89
Sasha Patsel <sashapatsel@sashas-mbp.attlocal.net>
2018-01-21 12:42:50 -0800

    added object. Now creating functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,15 @@
-var hangman = ["Michael Jordan", "Kobe Bryant", "Stephen Curry", "Hakeem Olajuwon", "Shaquille O'Neal", "LeBron James", "Tim Duncan", "Russel Westbrook", "Greg Poppovich", "Charles Barkley"]
+var hangman = {
+	p1: ["Michael Jordan", "_ _ _ _ _ _ _   _ _ _ _ _ _"],
+	p2: ["Kobe Bryant", "_ _ _ _   _ _ _ _ _ _"],
+	p3: ["Stephen Curry", "_ _ _ _ _ _ _   _ _ _ _ _"],
+	p4: ["Hakeem Olajuwon", "_ _ _ _ _ _   _ _ _ _ _ _ _ _"],
+	p5: ["Shaquille O'Neal", "_ _ _ _ _ _ _ _ _   _ _ _ _ _ _"],
+	p6: ["LeBron James", "_ _ _ _ _ _   _ _ _ _ _"],
+	p7: ["Tim Duncan", "_ _ _   _ _ _ _ _ _"],
+	p8: ["Russel Westbrook", "_ _ _ _ _ _   _ _ _ _ _ _ _ _ _"],
+	p9: ["Gregg Popovich", "_ _ _ _ _   _ _ _ _ _ _ _ _"],
+	p10: ["Charles Barkley", "_ _ _ _ _ _ _   _ _ _ _ _ _ _"]
+};
 
 
 // if (userInput === "k" || userInput === "o" || userInput === "b" || userInput === "e") {
@@ -6,3 +17,23 @@ var hangman = ["Michael Jordan", "Kobe Bryant", "Stephen Curry", "Hakeem Olajuwo
 // }
 
 //Will need to use toLowerCase()
+
+// if (correct guess) {
+// 	wins++
+// }
+
+// else {
+// 	function for adding guessed letters to list
+// }
+
+function scoreboard() {
+	
+}
+
+function playerName() {
+	for (var i = 0 ; i < hangman.p1[0].length ; i++) {
+		console.log(i);
+	}
+};
+
+playerName();
