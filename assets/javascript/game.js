0d4b7b3b35fb531d25dce6026883f323c10edd64
Sasha Patsel <sashapatsel@sashas-mbp.attlocal.net>
2018-01-21 16:55:27 -0800

    made game starting function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,14 @@
 var hangman = {
-	p1: ["Michael Jordan", "_ _ _ _ _ _ _   _ _ _ _ _ _"],
-	p2: ["Kobe Bryant", "_ _ _ _   _ _ _ _ _ _"],
-	p3: ["Stephen Curry", "_ _ _ _ _ _ _   _ _ _ _ _"],
-	p4: ["Hakeem Olajuwon", "_ _ _ _ _ _   _ _ _ _ _ _ _ _"],
-	p5: ["Shaquille O'Neal", "_ _ _ _ _ _ _ _ _   _ _ _ _ _ _"],
-	p6: ["LeBron James", "_ _ _ _ _ _   _ _ _ _ _"],
-	p7: ["Tim Duncan", "_ _ _   _ _ _ _ _ _"],
-	p8: ["Russel Westbrook", "_ _ _ _ _ _   _ _ _ _ _ _ _ _ _"],
-	p9: ["Gregg Popovich", "_ _ _ _ _   _ _ _ _ _ _ _ _"],
-	p10: ["Charles Barkley", "_ _ _ _ _ _ _   _ _ _ _ _ _ _"]
+	p1: ["Michael Jordan"],
+	p2: ["Kobe Bryant"],
+	p3: ["Stephen Curry"],
+	p4: ["Hakeem Olajuwon"],
+	p5: ["Shaquille O'Neal"],
+	p6: ["LeBron James"],
+	p7: ["Tim Duncan"],
+	p8: ["Russel Westbrook"],
+	p9: ["Gregg Popovich"],
+	p10: ["Charles Barkley"]
 };
 //Place holder for current word
 var player = "";
@@ -17,18 +17,17 @@ var wins = 0;
 
 var losses = 0;
 
-var guesses = 10;
-
 var startingPerson = 1;
 
 var amountPeople = 10;
 
 var wrongGuesses = [];
 
-var wordQueue = [];
+var wordQueue = "";
 
 var wordLetters = [];
 
+goodGuesses = [];
 // Steps to be completed:
 //1. reset button?
 //2.
@@ -37,64 +36,88 @@ function hangGame() {
 	wordQueue = hangman["p"+ startingPerson][0];
 	//splitting player names into letters
 	wordLetters = wordQueue.split('');
-	//The underscores
+	//Number of underscores
 	unguessed = wordLetters.length;
 	
+	amountPeople = 10;
+	wrongLetters =[];
+	goodGuesses =[];
+	possAnswers = ['a','b','c',
+					  'd','e','f',
+					  'g','h','i',
+					  'j','k','l',
+					  'm','n','o',
+					  'p','q','r',
+					  's','t','u',
+					  'v','w','x',
+					  'y','z'];
 
 
-document.getElementById("user-guesses").innerHTML = wrongGuesses;
-
-document.onkeyup = function() {
-	var userGuess = event.key;
-	if (userGuess === hangman.p1[0]) {
+	for(var i = 0; i< unguessed; i++) {
+		goodGuesses.push("_");
+		document.getElementById("the-word").innerHTML = goodGuesses;
 	}
+
+	document.getElementById("the-word").innerHTML = goodGuesses.join(" ");
+	document.getElementById("guesses-remain").innerHTML = amountPeople;
+	document.getElementById("wins").innerHTML = wins;
+	document.getElementById("losses").innerHTML = losses;
+	document.getElementById("user-guesses").innerHTML = wrongGuesses;
 };
 
-function scoreboard() {
-	var keepScore = guesses
-	document.getElementById("guesses-remain").innerHTML = keepScore
+hangGame()
 
-}
+// document.onkeyup = function() {
+// 	var userGuess = event.key;
+// 	if (userGuess === hangman.p1[0]) {
+// 	}
+// };
 
-//Displays incorrectly guessed letter on screen
-function usedLetters() {
-	var userGuess = event.key
+// function scoreboard() {
+// 	var keepScore = guesses
+// 	document.getElementById("guesses-remain").innerHTML = keepScore
 
-	if (userGuess !== player.toLowerCase().indexOf( String.fromCharCode( e.which )) {
-		document.getElementById("user-guesses").innerHTML = userGuess
-	}
-}
+// }
 
-function playerName() {
-	for (var i = 0 ; i < hangman.p1[0].length ; i++) {
-		console.log(i);
-	}
-};
+// //Displays incorrectly guessed letter on screen
+// function usedLetters() {
+// 	var userGuess = event.key
 
-function checkAnswer(userInput){
-	if (quiz["q"+ startingQuestion][1]===userInput){
-		score = score + 1;
+// 	if (userGuess !== player.toLowerCase().indexOf( String.fromCharCode( e.which )) {
+// 		document.getElementById("user-guesses").innerHTML = userGuess
+// 	}
+// }
 
-		nextPerson()
-		reloadInfo()
-	}else{
-		nextPerson()
-		reloadInfo()
-	}
-}
+// function playerName() {
+// 	for (var i = 0 ; i < hangman.p1[0].length ; i++) {
+// 		console.log(i);
+// 	}
+// };
 
-function reloadInfo(){
-	var scoreHtml = "<p>SCORE: " + score + "</p>"
-	document.querySelector("#scorePlace").innerHTML = scoreHtml;
+// function checkAnswer(userInput){
+// 	if (quiz["q"+ startingQuestion][1]===userInput){
+// 		score = score + 1;
+
+// 		nextPerson()
+// 		reloadInfo()
+// 	}else{
+// 		nextPerson()
+// 		reloadInfo()
+// 	}
+// }
+
+// function reloadInfo(){
+// 	var scoreHtml = "<p>SCORE: " + score + "</p>"
+// 	document.querySelector("#scorePlace").innerHTML = scoreHtml;
 	 
-	var questionHtml = quiz["p"+ startingQuestion][0]
-	document.querySelector("#questionPlace").innerHTML = questionHtml;
-}
+// 	var questionHtml = quiz["p"+ startingQuestion][0]
+// 	document.querySelector("#questionPlace").innerHTML = questionHtml;
+// }
 
-function nextPerson(){
-	if(startingPerson <= amountPeople){
-		startingPerson++;
-	}
+// function nextPerson(){
+// 	if(startingPerson <= amountPeople){
+// 		startingPerson++;
+// 	}
 	// else{
 
 	// 	var scoreHtml = "<h1>GAME OVER</h1>"
@@ -102,7 +125,7 @@ function nextPerson(){
 	// 	document.querySelector("#questionPlace").innerHTML = questionHtml;
 	// }
 	
-};
+// };
 
 //Notes:
 
@@ -117,5 +140,15 @@ function nextPerson(){
 // else {
 // 	function for adding guessed letters to list
 // }
-
-//document.getElementById("id").innerHTML
+// var hangman = {
+// 	p1: ["Michael Jordan", "_ _ _ _ _ _ _   _ _ _ _ _ _"],
+// 	p2: ["Kobe Bryant", "_ _ _ _   _ _ _ _ _ _"],
+// 	p3: ["Stephen Curry", "_ _ _ _ _ _ _   _ _ _ _ _"],
+// 	p4: ["Hakeem Olajuwon", "_ _ _ _ _ _   _ _ _ _ _ _ _ _"],
+// 	p5: ["Shaquille O'Neal", "_ _ _ _ _ _ _ _ _   _ _ _ _ _ _"],
+// 	p6: ["LeBron James", "_ _ _ _ _ _   _ _ _ _ _"],
+// 	p7: ["Tim Duncan", "_ _ _   _ _ _ _ _ _"],
+// 	p8: ["Russel Westbrook", "_ _ _ _ _ _   _ _ _ _ _ _ _ _ _"],
+// 	p9: ["Gregg Popovich", "_ _ _ _ _   _ _ _ _ _ _ _ _"],
+// 	p10: ["Charles Barkley", "_ _ _ _ _ _ _   _ _ _ _ _ _ _"]
+// };
